483908b26bf492dc4588ce6e1d5ea7deed5627c1
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-08-24 20:26:58 +0200

    design post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=30000)
+    publish_date = models.DateTimeField(default=timezone.now)
+    
+
+    
+
