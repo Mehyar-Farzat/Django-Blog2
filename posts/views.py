16fdf3f4a6d2223699297d4b984202d3a63ef5ec
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-05 13:28:15 +0200

    create html file and url

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .forms import PostForm
 
 
 # Create your views here.
+def create_post(request):
+    form = PostForm()
+    return render(request,'create_post.html',{'form':form})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import create_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create_post/', create_post, name='create_post'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
